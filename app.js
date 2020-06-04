c5c992325bb7f985881654a6f9c88efb3044fd17
IAmNini <fortuny.carrega@gmail.com>
2020-06-04 23:22:53 +0200

    no ricerche

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function main() {
   const voto3 = document.getElementById('voto3') // vale 33.33%
   const voto4 = document.getElementById('voto4') // vale 33.33%
   const voto5 = document.getElementById('voto5') // vale 100%
-  const voto6 = document.getElementById('voto6') // vale 100%
   const voto7 = document.getElementById('voto7') // vale 200%
   const submitButton = document.querySelector('button')
   const risultato = document.getElementById('risultato')
@@ -14,7 +13,7 @@ function main() {
 
   submitButton.addEventListener('click', (event) => {
     event.preventDefault()
-    const media = (parseInt(voto1.value) * 10 + parseInt(voto2.value) * 3.333 + parseInt(voto3.value) * 3.333 + parseInt(voto4.value) * 3.333 + parseInt(voto5.value) * 10 + parseInt(voto6.value) * 10 + parseInt(voto7.value) * 20) / 59.999
+    const media = (parseInt(voto1.value) * 10 + parseInt(voto2.value) * 3.333 + parseInt(voto3.value) * 3.333 + parseInt(voto4.value) * 3.333 + parseInt(voto5.value) * 10 + parseInt(voto7.value) * 20) / 49.999
     risultato.innerHTML = nomeAlunno.value + ' ha la media del ' + media.toFixed(2) + ' in Italiano.'
     nuovaMedia.classList.remove('nuova-media')
   })
